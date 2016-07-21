c97693a3fea6dda9707a11bc8949bedd8b590677
lohithkumar24 <lohithkumar24@gmail.com>
2016-07-21 14:47:37 +0530

    Took the reviewer's suggestion to heart. Used a single method to perform the task for all the buttons

--- app/src/main/java/com/example/android/ksappportfolios/MainActivity.java
+++ app/src/main/java/com/example/android/ksappportfolios/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.android.ksappportfolios;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,45 +16,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    //Method to show toast on Popular Movies button click
-    public void showToastPopularMovies(View view) {
-        Toast toast = Toast.makeText(this, "This button will launch my " + "Popular Movies " + "App", Toast.LENGTH_SHORT);
+    //This method is called when any of the buttons are clicked
+    public void displayMessage(View view){
+        Button button = (Button) view;
+        String buttonText = (String) button.getText();
+
+        Context context = getApplicationContext();
+        CharSequence text = getString(R.string.open_app) +buttonText;
+
+        Toast toast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
         toast.show();
         toast.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 0);
     }
 
-    //Method to show toast on Stock Hawk button click
-    public void showToastStockHawk(View view) {
-        Toast toast = Toast.makeText(this, "This button will launch my " + "Stock Hawk " + "App", Toast.LENGTH_SHORT);
-        toast.show();
-        toast.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 0);
-    }
-
-    //Method to show toast on Build It Bigger button click
-    public void showToastBuildItBigger(View view) {
-        Toast toast = Toast.makeText(this, "This button will launch my " + "Build it Bigger " + "App", Toast.LENGTH_SHORT);
-        toast.show();
-        toast.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 0);
-    }
-
-    //Method to show toast on Make Your App Material button click
-    public void showToastMakeYourAppMaterial(View view) {
-        Toast toast = Toast.makeText(this, "This button will launch my " + "Make Your App Material " + "App", Toast.LENGTH_SHORT);
-        toast.show();
-        toast.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 0);
-    }
-
-    //Method to show toast on Go Ubiquitous button click
-    public void showToastGoUbiquitous(View view) {
-        Toast toast = Toast.makeText(this, "This button will launch my " + "Go Ubiquitous " + "App", Toast.LENGTH_SHORT);
-        toast.show();
-        toast.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 0);
-    }
-
-    //Method to show toast on Capstone button click
-    public void showToastCapstone(View view) {
-        Toast toast = Toast.makeText(this, "This button will launch my " + "Capstone " + "App", Toast.LENGTH_SHORT);
-        toast.show();
-        toast.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 0);
-    }
 }
+
+
